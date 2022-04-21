e5751f70fa01708d30fda85ee4e79def444d5fa5
CrimsonCoder42 <nostro37@gmail.com>
2022-04-21 16:55:17 -0400

    JSX demo done

--- index.js
+++ index.js
@@ -1,53 +1,31 @@
-function Navbar() {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="#">Navbar</a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
 
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav mr-auto">
-                <li className="nav-item active">
-                    <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#">Link</a>
-                </li>
-                <li className="nav-item dropdown">
-                    <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Dropdown
-                    </a>
-                    <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a className="dropdown-item" href="#">Action</a>
-                    <a className="dropdown-item" href="#">Another action</a>
-                    <div className="dropdown-divider"></div>
-                    <a className="dropdown-item" href="#">Something else here</a>
-                    </div>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link disabled" href="#">Disabled</a>
-                </li>
-                </ul>
-                <form className="form-inline my-2 my-lg-0">
-                    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-                    <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                </form>
-            </div>
-        </nav>
-    )
-}
+const page = (
+    <div> 
+    <h1 className = "header"> This is JSX </h1>
+    <p>This is a paragraph </p>
+    
+    </div>
+)
 
-function MainContent() {
-    return(
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.  </p>
-    )
-}
+
+const navBar = (
+    <div> 
+    
+    <nav> 
+        <h1>DevCo</h1>
+            <ul>
+                <li>Pricing </li>
+                <li>About </li>
+                <li>Contact </li>
+            </ul>
+    </nav>
+    
+    </div>
+)
+
+const combo = [navBar, page]
+
 
 ReactDOM.render(
-    <div>
-    <Navbar />
-    <MainContent />
-    </div>,
-    document.getElementById("root")
-)
+    combo
+    , document.getElementById("root"))
